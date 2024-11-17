71091d879e1b8dfd6a486fa4feda5badf6102768
Meshi Itzhaki <139878965+MeshiIthzaki@users.noreply.github.com>
2024-11-17 17:33:35 +0200

    stereo vision camera calibration

---
+++ stereovision.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2 as cv
+
+# Load stereo map parameters
+cv_file = cv.FileStorage()
+cv_file.open('stereoMap.xml', cv.FileStorage_READ)
+
+stereoMapL_x = cv_file.getNode('stereoMapL_x').mat()
+stereoMapL_y = cv_file.getNode('stereoMapL_y').mat()
+stereoMapR_x = cv_file.getNode('stereoMapR_x').mat()
+stereoMapR_y = cv_file.getNode('stereoMapR_y').mat()
+cv_file.release()
+
+# Load the images
+left_image_path = "images/stereoLeft/left_12.png"
+right_image_path = "images/stereoRight/right_12.png"
+
+frame_left = cv.imread(left_image_path)
+frame_right = cv.imread(right_image_path)
+
+if frame_left is None or frame_right is None:
+    print("Error: Could not load one or both images.")
+    exit()
+
+# Undistort and rectify the images
+rectified_left = cv.remap(frame_left, stereoMapL_x, stereoMapL_y, cv.INTER_LANCZOS4, cv.BORDER_CONSTANT, 0)
+rectified_right = cv.remap(frame_right, stereoMapR_x, stereoMapR_y, cv.INTER_LANCZOS4, cv.BORDER_CONSTANT, 0)
+
+# Display the rectified images
+cv.imshow("Rectified Left Image", rectified_left)
+cv.imshow("Rectified Right Image", rectified_right)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
